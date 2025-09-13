d78012fd531c0a4999968f5532981dee01babbf1
Markus <neets018@gmail.com>
2025-09-13 19:05:03 +0200

    initial

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int motors() {
+  int motorInput;
+  printf("Enter the number of motors to carry the packages: ");
+  scanf("%d", &motorInput);
+  return motorInput;
+}
+
+int mass() {
+  int massInput;
+  printf("Enter the total mass of packages: ");
+  scanf("%d", &massInput);
+  return massInput;
+}
+
+int main() {
+  float capacityPerMotor = 5.6;
+  float totalCapacity = motors() * capacityPerMotor;
+  if (mass() <= totalCapacity) {
+    printf("Yes! The conveyor belt can carry the packages.\n");
+  } else {
+    printf("No. The conveyor belt cannot carry the packages.\n");
+  }
+  return 0;
+}
